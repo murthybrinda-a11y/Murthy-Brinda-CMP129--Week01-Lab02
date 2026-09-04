3ccd98a1512477707e6fb470fb5c757f83d5fe59
murthy.brinda <murthy.brinda@student.ccm.edu>
2026-09-04 14:29:58 -0400

    Lab2

--- Patterns.java
+++ Patterns.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 package Labs.Week_1.scr;
 
-public class Patterns {
+public class Patterns 
+
+{
+    public static void main( String[] args)
+    {
+        int stores ;
+        Scanner keyboard = new Scanner(System.in);
+        
+
+    }
+
     
 }
--- SalesBarChart.java
+++ SalesBarChart.java
@@ -1,5 +1,23 @@
 package Labs.Week_1.scr;
 
-public class SalesBarChart {
-    
+import java.util.Scanner;
+
+public class SalesBarChart 
+{
+    public static void main( String[] args)
+    {
+        int Numastricks ;
+        int NumSales ;
+        int Numstores;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the number of number of stores");
+        Numstores = keyboard.nextInt();
+        for(int i = 0 ; i < Numstores; i++)
+        {
+            
+        }
+
+
+        
+    }
 }
