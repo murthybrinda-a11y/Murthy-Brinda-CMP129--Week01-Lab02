d43a043b9b22c6558a3fe5328680b8ff8e3a65b7
murthy.brinda <murthy.brinda@student.ccm.edu>
2026-09-08 00:06:44 -0400

    Lab2 complete

--- Patterns.java
+++ Patterns.java
@@ -1,12 +1,40 @@
-import java.util.Scanner;
 
 public class Patterns 
 
 {
     public static void main( String[] args)
     {
-        int stores ;
-        Scanner keyboard = new Scanner(System.in);
+        System.out.println("------------------------------");
+        System.out.println("          Pattern A           ");
+        System.out.println("------------------------------");
+
+        //Pattern A
+        for(int i = 1 ; i <= 10; i++)
+        {
+            System.out.println("");
+            for (int k = 1; k <= i; k++) 
+            {
+                System.out.print("+"); 
+            }
+        }
+        System.out.println();
+        
+        //Pattern B    
+        System.out.println("------------------------------");
+        System.out.println("          Pattern B           ");
+        System.out.println("------------------------------");
+
+         for(int i = 10 ; i >= 1; i--)
+        {
+            System.out.println("");
+            for (int k = 1; k <= i; k++) 
+            {
+                System.out.print("+"); 
+            }
+        }
+        System.out.println();
+
+
 
 
     }
--- SalesBarChart.java
+++ SalesBarChart.java
@@ -5,16 +5,33 @@ public class SalesBarChart
     public static void main( String[] args)
     {
         int Numastricks ;
-        int NumSales ;
-        int Numstores;
+        int numOfStores;
         Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter the number of number of stores");
-        Numstores = keyboard.nextInt();
-        for(int i = 0 ; i < Numstores; i++)
+        System.out.print("Enter number of stores");
+        numOfStores = keyboard.nextInt();
+ // Arrays 
+        int[] StoreArr = new int[numOfStores];
+        int[] NumastricksArr = new int[numOfStores];
+//Loop to ask numOfStores
+        for(int i = 0 ; i < numOfStores; i++)
         {
-
+            System.out.print("Enter today's sales for store " + (i+1) + " ");
+            StoreArr[i] = keyboard.nextInt();
+            NumastricksArr[i] = StoreArr[i]/100;
         }
+        System.out.println("\nSALES BAR CHART\n ");
+        System.out.print("Each * = 100\n");
+// Loop to print everything
+        for (int i = 0 ; i < numOfStores; i++)
+        {
+            System.out.print("Store " + ( i + 1)+  ": ");
+            for(int k = 0; k < NumastricksArr[i]; k ++)
+            {
+                System.out.print("*");
+            }
+            System.out.print('\n');
 
+        }  
 
         
     }
